41e4714133daeef99fe64f4f312daae5b473d459
zef1n <icw20@mail.ru>
2024-02-08 22:25:48 +0500

    Яндекс лицей 08.02.24 - Добавлены задачи по теме "Функции передача параметров" - Добавлены задачи по теме "Функции с переменным числом аргументов"

--- Функции передача параметров/main.py
+++ Функции передача параметров/main.py
@@ -16,15 +16,15 @@ def count_food(days):
 
 
 # Покажите отличие
-# sp_sort = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
-# sp_sort.sort()
-# print("Список после использования метода sort():", sp_sort)
-#
-# sp = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
-# sp_sorted = sorted(sp)
-#
-# print("Отсортированный список с использованием функции sorted():", sp_sorted)
-# print("Исходный список после использования функции sorted():", sp)
+sp_sort = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
+sp_sort.sort()
+print("Список после использования метода sort():", sp_sort)
+
+sp = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
+sp_sorted = sorted(sp)
+
+print("Отсортированный список с использованием функции sorted():", sp_sorted)
+print("Исходный список после использования функции sorted():", sp)
 
 
 # Продолжите ряд
@@ -41,20 +41,54 @@ def mirror(arr):
 
 
 # Фрактальный список — 1
+fractal = []
+fractal.extend([0, fractal, fractal, 2])
 
-fractal = [0, 1, 2, 3]
-fractal = [0, fractal, fractal, 2]
 
 # От перестановки мест
 
 # Что ты имела в виду?
+numbers = [2, 5, 7, 7, 8, 4, 1, 6]
+odd = []
+even = []
+
+for number in numbers:
+    if number % 2 == 0:
+        even.append(number)
+    else:
+        odd.append(number)
+
+print("Четные числа:", even)
+print("Нечетные числа:", odd)
+
 
 # Домашняя работа
 # Числа в строке
+def from_string_to_list(string, container):
+    numbers = [int(num) for num in string.split()]
+    container.extend(numbers)
+
 
 # Билеты
+def allocation(data):
+    not_alloc = []
+
+    for rad, mest in data:
+        if places[rad - 1][mest - 1] == 0:
+
+            places[rad - 1][mest - 1] = 1
+        else:
+            not_alloc.append((rad, mest))
+    return not_alloc
+
 
 # Транспонирование
+def transpose(matrix):
+    stroki = len(matrix)
+    koloni = len(matrix[0])
+    tr_matrix = [[matrix[j][i] for j in range(stroki)] for i in range(koloni)]
+    matrix.clear()
+    matrix.extend(tr_matrix)
 
 # Дополнительные задачи
 # Обмен личностями
